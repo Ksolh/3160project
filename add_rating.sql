1459bd48d7711f92b902efc9704c79be43ea781e
Alazar49 <79599152+Alazar49@users.noreply.github.com>
2021-05-05 12:23:50 -0400

    Delete add_rating.sql

--- add_rating.sql
+++
@@ -1,7 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `add_rating`(in rating_id int(11), in person_id int(11), restaurant_id int(11), driver_id int(11), score FLOAT, descr VARCHAR(75))
-BEGIN
-
-insert into rating (rating_id, person_id, restaurant_id, driver_id, score, comment) 
-values(rating_id, person_id, restaurant_id, driver_id, score, comment);
-
-END
